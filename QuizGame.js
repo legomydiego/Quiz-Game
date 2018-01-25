b7b0a7db823f46f91ce1f456f7c208972973a857
Diego Vargas <diegofvargas@gmail.com>
2018-01-25 13:41:16 -0500

    Added closure for score tracking

--- QuizGame.js
+++ QuizGame.js
@@ -10,28 +10,52 @@
             console.log(i + ') ' + this.answers[i]);
         }
     }
-    Question.prototype.checkAnswer = function(answer) {
+    Question.prototype.checkAnswer = function(answer,callback) {
+        var sc;
         if(answer === this.correctAnswer) {
-            console.log('That is correct!')
-            score++;
-            console.log(`Your current score is ${score}`);
+            console.log('That is correct!');
+            sc = callback(true);
+
         } else {
             console.log('Sorry wrong answer');
-            console.log(`Your current score is ${score}`);
+            sc = callback(false);
         }
+        this.displayScore(sc);
+    }
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is ' + score);
+        console.log('-------------------');
     }
     
     var first = new Question('Is bitcoin the future?',['yes','no'],0);
     var second = new Question('Where would you rank Lebron?',['top 3','top 5','top 10','top 20'],0);
     var third = new Question('What is the best day at the gym?',['chest day','back day','leg day'],2);
     var fourth = new Question('What has been the best trip I have taken?',['Spain','Argentina','Japan','Italy'],2);
-    
     var questionArray = [first,second,third,fourth];
-    var score = 0;
-    var questionIndex = Math.floor(Math.random()*4);
-    questionArray[questionIndex].displayQuestion();
-    var response = parseInt(prompt('What\'s the answer?'));
-    questionArray[questionIndex].checkAnswer(response);
+
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if(correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
+
+    var keepScore = score();
+    
+    function nextQuestion(){
+        var questionIndex = Math.floor(Math.random()*4);
+        questionArray[questionIndex].displayQuestion();
+        var response = prompt('What\'s the answer?');
+        
+        if(response !== 'exit') {
+            questionArray[questionIndex].checkAnswer(parseInt(response),keepScore);
+            nextQuestion();
+        }
+    }
+    nextQuestion();
 }) ();
 
 /*
